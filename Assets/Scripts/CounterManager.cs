c5b8488edba4f20d8ff50668f1abdfc2b0f42f15
sein1423 <kun3650@gmail.com>
2022-06-06 00:47:08 +0900

    setting

---
+++ Assets/Scripts/CounterManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class CounterManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void goIllust()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void goShop()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainManager : MonoBehaviour
+{
+    public GameManager gm;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void GameStart()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
