9bd77e778b4ebf14a0c8b1d3b5114962d09e1682
sein1423 <kun3650@gmail.com>
2022-06-16 15:49:44 +0900

    오더 판단

--- Assets/Scripts/orderSpawner.cs
+++ Assets/Scripts/orderSpawner.cs
@@ -9,6 +9,9 @@ public class orderSpawner : MonoBehaviour
     float Timefloat = 0;
     public GameObject orderObject;
     public GameManager gm;
+    int randomOrder;
+    bool ordercom = false;
+    
     public struct order
     {
         public string state;
@@ -18,14 +21,18 @@ public class orderSpawner : MonoBehaviour
     }
 
 
-    public order[] orders = new order[3];
+    public order[] orders = new order[4];
     // Start is called before the first frame update
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
 
 
 
     void Start()
     {
-        orderObject.SetActive(false);
+        orderObject.SetActive(ordercom);
         gm = GameObject.Find("GameManager").GetComponent<GameManager>();
         orders[0].state = "파란 꽃으로 모아 주세요";
         orders[0].lang = null;
@@ -42,6 +49,12 @@ public class orderSpawner : MonoBehaviour
         orders[2].color = null;
         orders[2].season = null;
 
+        orders[3].state = "프리미엄 5송이 주세요"; 
+        orders[3].lang = "우정";
+        orders[3].color = "yellow";
+        orders[3].season = "spring";
+
+
     }
 
     // Update is called once per frame
@@ -51,22 +64,53 @@ public class orderSpawner : MonoBehaviour
         
         if(Timefloat > 3.0 && !orderObject.activeSelf)
         {
+            ordercom = false;
+            randomOrder = Random.Range(0, orders.Length);
+        }
+
+        if (!ordercom)
+        {
             newOrder();
         }
     }
 
     public void newOrder()
     {
-
-        int randomOrder = Random.Range(0, orders.Length);
         orderObject.SetActive(true);
         ordertext.text = orders[randomOrder].state;
     }
 
     public void orderComplite()
     {
+        int score = 0;
+        if(orders[randomOrder].lang != null)
+        {
+            if(orders[randomOrder].lang == gm.bou1.lang)
+            {
+                score += 10;
+            }
+        }
+
+        if(orders[randomOrder].color != null)
+        {
+            if (orders[randomOrder].color == gm.bou1.color)
+            {
+                score += 10;
+            }
+        }
+
+        if(orders[randomOrder].season != null)
+        {
+            if (orders[randomOrder].season == gm.bou1.season)
+            {
+                score += 10;
+            }
+        }
+
+
         orderObject.SetActive(false);
         Timefloat = 0;
-        gm.money += 10;
+        gm.money += score;
+        
     }
 }
