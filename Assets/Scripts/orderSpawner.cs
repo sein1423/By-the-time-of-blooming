f6ee77b791c6cbb803d11339ef4bf62f29af63fb
sein1423 <kun3650@gmail.com>
2022-06-16 15:34:17 +0900

    order추가

--- Assets/Scripts/orderSpawner.cs
+++ Assets/Scripts/orderSpawner.cs
@@ -8,10 +8,40 @@ public class orderSpawner : MonoBehaviour
     public Text ordertext;
     float Timefloat = 0;
     public GameObject orderObject;
+    public GameManager gm;
+    public struct order
+    {
+        public string state;
+        public string lang;
+        public string color;
+        public string season;
+    }
+
+
+    public order[] orders = new order[3];
     // Start is called before the first frame update
+
+
+
     void Start()
     {
         orderObject.SetActive(false);
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+        orders[0].state = "파란 꽃으로 모아 주세요";
+        orders[0].lang = null;
+        orders[0].color = "blue";
+        orders[0].season = null;
+
+        orders[1].state = "여름에 피는 꽃으로 주세요";
+        orders[1].lang = null;
+        orders[1].color = null;
+        orders[1].season = "summer";
+
+        orders[2].state = "꽃말이 사랑인 꽃으로 주세요";
+        orders[2].lang = "사랑";
+        orders[2].color = null;
+        orders[2].season = null;
+
     }
 
     // Update is called once per frame
@@ -19,10 +49,24 @@ public class orderSpawner : MonoBehaviour
     {
         Timefloat += Time.deltaTime;
         
-        if(Timefloat > 3.0)
+        if(Timefloat > 3.0 && !orderObject.activeSelf)
         {
-            orderObject.SetActive(true);
-            ordertext.text = "주문이용";
+            newOrder();
         }
+    }
+
+    public void newOrder()
+    {
+
+        int randomOrder = Random.Range(0, orders.Length);
+        orderObject.SetActive(true);
+        ordertext.text = orders[randomOrder].state;
+    }
+
+    public void orderComplite()
+    {
+        orderObject.SetActive(false);
+        Timefloat = 0;
+        gm.money += 10;
     }
 }
